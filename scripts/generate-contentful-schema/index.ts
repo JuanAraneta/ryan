4010756b07c84787be0b328619d398e6e250a677
Hernan Case <hercase92@gmail.com>
2025-07-21 17:43:41 -0300

    chore: improve code formatting by adding missing semicolons in Contentful schema generation script

--- scripts/generate-contentful-schema/index.ts
+++ scripts/generate-contentful-schema/index.ts
@@ -1,12 +1,12 @@
-import * as dotenv from "dotenv"
-import { $ } from "zx"
+import * as dotenv from "dotenv";
+import { $ } from "zx";
 
-;(async () => {
-  dotenv.config()
+(async () => {
+  dotenv.config();
 
-  const gqlUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}`
-  const header = `Authorization: Bearer ${process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN}`
+  const gqlUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}`;
+  const header = `Authorization: Bearer ${process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN}`;
 
-  await $`npx gql-tada generate-schema ${gqlUrl} --header ${header}`
-  await $`gql.tada generate-output`
-})()
+  await $`npx gql-tada generate-schema ${gqlUrl} --header ${header}`;
+  await $`gql.tada generate-output`;
+})();
