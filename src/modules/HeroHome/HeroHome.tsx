a5c9aa48068856257fc77ca99fe2f546e921941e
Hernan Case <hercase92@gmail.com>
2025-07-24 19:16:44 -0300

    refactor: replace routing card implementation with HeroRoutingCard component in HeroHome

--- src/modules/HeroHome/HeroHome.tsx
+++ src/modules/HeroHome/HeroHome.tsx
@@ -1,8 +1,7 @@
 import { AIChatPrompt } from "@/components/core/AIChat";
 import { RichTitleText } from "@/components/core/RichText";
 import { Section } from "@/components/core/Section";
-import { ZoomImage } from "@/components/core/ZoomImage";
-import { MdKeyboardBackspace } from "react-icons/md";
+import { HeroRoutingCard } from "./components/HeroRoutingCard";
 
 export interface HeroHomeProps {
   headline: string; // max 70 chars
@@ -35,27 +34,9 @@ export function HeroHome({ headline, routingCards, prompts }: HeroHomeProps) {
           <AIChatPrompt prompts={prompts} />
         </div>
 
-        {/* Routing Cards */}
         <div className="flex flex-col dsk:flex-row">
           {routingCards.map((card, idx) => (
-            <a key={idx} href={card.href} className="flex-1 group relative">
-              <ZoomImage
-                src={card.imageUrl}
-                alt={card.eyebrow}
-                className="aspect-21/9"
-              />
-
-              <div className="flex-col py-6 px-10 gap-3 contents dsk:flex">
-                <p className="flex items-center justify-between gap-2 absolute bottom-[1.12rem] dsk:static w-full px-4 dsk:px-0">
-                  <span className="typo-eyebrow">{card.eyebrow}</span>
-                  <MdKeyboardBackspace className="w-6 h-6 rotate-180 dsk:group-hover:translate-x-2 transition-transform duration-100" />
-                </p>
-
-                <span className="typo-heading-4 font-light hidden dsk:block">
-                  {card.subheading}
-                </span>
-              </div>
-            </a>
+            <HeroRoutingCard key={idx} {...card} />
           ))}
         </div>
       </Section>
