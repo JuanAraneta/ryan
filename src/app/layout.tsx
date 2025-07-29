3efb8b3a0e98bc8fe374ac92b9c172617db665ac
Hernan Case <hercase92@gmail.com>
2025-07-29 18:46:30 -0300

    refactor: removed unused metadata from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,4 @@
-import type { Metadata } from "next";
 import "../global.css";
-
-export const metadata: Metadata = {
-  title: "Ryan Tax Firm",
-  description:
-    "Ryan is a global tax services, software, and technology firm providing an integrated suite of federal, state, local, and international tax services to companies across the world.",
-};
 
 export default async function RootLayout({
   children,
