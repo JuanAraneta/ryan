8a53a69cf9a0018f72a8797537a45541ebd3a463
Hernan Case <hercase92@gmail.com>
2025-07-29 16:22:38 -0300

    feat: enhance shortTextFieldFactory to support array type

--- src/models/factories/shortTextFieldFactory.ts
+++ src/models/factories/shortTextFieldFactory.ts
@@ -2,17 +2,32 @@ import { DeepPartial } from "@/types/utils/DeepPartial";
 import { ExpandedFieldDetails } from "../types/ExpandedFieldDetails";
 import merge from "lodash/merge";
 
-export const shortTextFieldFactory = (
-  props: {
-    id: string;
-    name: string;
-  } & DeepPartial<Omit<ExpandedFieldDetails, "id" | "name" | "type">>,
-): ExpandedFieldDetails =>
-  merge({
-    type: "Symbol",
-    editorInterface: {
-      widgetId: "singleLine",
-      widgetNamespace: "builtin",
-    },
-    ...props,
-  });
+type ShortTextFieldFactoryProps = {
+  id: string;
+  name: string;
+} & (
+  | { array?: false; size?: undefined }
+  | { array: true; size: { min?: number; max: number } }
+);
+
+type PartialFieldDetails = Omit<ExpandedFieldDetails, "id" | "name" | "type">;
+
+export const shortTextFieldFactory = ({
+  array = false,
+  size,
+  ...props
+}: ShortTextFieldFactoryProps &
+  DeepPartial<PartialFieldDetails>): ExpandedFieldDetails => {
+  return merge(
+    { editorInterface: { widgetNamespace: "builtin" } } as PartialFieldDetails,
+    array
+      ? {
+          type: "Array",
+          validations: [{ size }],
+          items: { type: "Symbol" },
+          editorInterface: { widgetId: "tagEditor" },
+        }
+      : { type: "Symbol", editorInterface: { widgetId: "singleLine" } },
+    props,
+  );
+};
