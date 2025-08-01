fdc6d087d5dad6ee96e8e0ec08b1b10aa7e1473e
Hernan Case <hercase92@gmail.com>
2025-08-01 14:09:37 -0300

    refactor: enhance iconFieldFactory with additional settings and props

--- src/models/factories/iconFieldFactory.ts
+++ src/models/factories/iconFieldFactory.ts
@@ -1,18 +1,28 @@
-import { ExpandedFieldDetails } from "../types/ExpandedFieldDetails";
 import merge from "lodash/merge";
+import { ExpandedFieldDetails } from "../types/ExpandedFieldDetails";
 import { Icons } from "@/components/icons";
 
 export const iconFieldFactory = ({
   id = "icon",
   name = "Icon",
+  ...props
 }: {
   id?: string;
   name?: string;
 }): ExpandedFieldDetails =>
-  merge({
-    id,
-    name,
-    type: "Symbol",
-    validations: [{ in: Object.keys(Icons) }],
-    editorInterface: { widgetId: "dropdown", widgetNamespace: "builtin" },
-  });
+  merge(
+    {
+      id,
+      name,
+      type: "Symbol",
+      validations: [{ in: Object.keys(Icons) }],
+      editorInterface: {
+        widgetId: "dropdown",
+        widgetNamespace: "builtin",
+        settings: {
+          helpText: "Select an icon from the list",
+        },
+      },
+    },
+    props,
+  );
