ef789770b4388e02fe67e618486c4970cc677cdd
Hernan Case <hercase92@gmail.com>
2025-08-04 15:30:48 -0300

    feat: add DRAFT_MODE_SECRET to .env.template and enable draft mode in page query

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -4,16 +4,20 @@ import { contentClient } from "@/lib/contentful/contentClient";
 import { GetPageBySlugAndMarketQuery } from "@/lib/contentful/query/GetPageBySlugAndMarketQuery";
 import { readFragment } from "gql.tada";
 import { PageModulesCollectionFragment } from "@/lib/contentful/fragments/PageModulesCollectionFragment";
+import { draftMode } from "next/headers";
 
 export default async function Page(props: {
   params: Promise<{ slug: string[] }>;
 }) {
   const params = await props.params;
+  const draft = await draftMode();
+
   const slugs = Array.isArray(params.slug) ? params.slug : [params.slug];
 
   const [marketSlug, locale, slug] = slugs;
 
   const pageResult = await contentClient.query(GetPageBySlugAndMarketQuery, {
+    preview: draft.isEnabled,
     marketSlug,
     locale,
     slug,
---
+++ src/app/app/api/draft/route.ts
@@ -0,0 +1,40 @@
+import { contentClient } from "@/lib/contentful/contentClient";
+import { GetAllPagesQuery } from "@/lib/contentful/query/GetAllPagesQuery";
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+
+const SECRET = process.env.DRAFT_MODE_SECRET;
+
+if (!SECRET) {
+  throw new Error("DRAFT_MODE_SECRET environment variable is not configured");
+}
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const secret = searchParams.get("secret");
+  const slug = searchParams.get("slug");
+
+  if (secret !== SECRET || !slug) {
+    return new Response("Invalid token", { status: 401 });
+  }
+
+  const pagesResult = await contentClient.query(GetAllPagesQuery, {});
+
+  const page = pagesResult.data?.pageCollection?.items.find(
+    (page) => page?.slug === slug,
+  );
+
+  // If the slug doesn't exist prevent draft mode from being enabled
+  if (!page?.slug) {
+    return new Response("Invalid slug", { status: 401 });
+  }
+
+  // Enable Draft Mode by setting the cookie
+  const draft = await draftMode();
+  draft.enable();
+
+  // Redirect to the path from the fetched post
+  // We don't redirect to searchParams.slug as that might lead to open redirect vulnerabilities
+  redirect(page.slug);
+}
